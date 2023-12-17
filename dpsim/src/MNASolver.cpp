4aaabe9232ae8fb94e8b52f9e370a3c9c77763ae
martin.moraga <martin.moraga@rwth-aachen.de>
2023-12-17 17:15:18 +0100

    fix small logging error in function collectVirtualNodes
    Signed-off-by: martin.moraga <martin.moraga@rwth-aachen.de>

--- dpsim/src/MNASolver.cpp
+++ dpsim/src/MNASolver.cpp
@@ -434,18 +434,23 @@ template <typename VarType> void MnaSolver<VarType>::collectVirtualNodes() {
     if (pComp->hasVirtualNodes()) {
       for (UInt node = 0; node < pComp->virtualNodesNumber(); ++node) {
         mNodes.push_back(pComp->virtualNode(node));
-        SPDLOG_LOGGER_INFO(mSLog, "Collected virtual node {} of {}",
+        SPDLOG_LOGGER_INFO(mSLog,
+                           "Collected virtual node {} of {} of component {}",
                            virtualNode, node, pComp->name());
       }
     }
 
     // Repeat the same steps for virtual nodes of sub components
     // TODO: recursive behavior
+    std::cout << "SubCompoenent number = " << pComp->subComponents().size()
+              << std::endl;
     if (pComp->hasSubComponents()) {
+      std::cout << "TestSubComp" << std::endl;
       for (auto pSubComp : pComp->subComponents()) {
         for (UInt node = 0; node < pSubComp->virtualNodesNumber(); ++node) {
           mNodes.push_back(pSubComp->virtualNode(node));
-          SPDLOG_LOGGER_INFO(mSLog, "Collected virtual node {} of {}",
+          SPDLOG_LOGGER_INFO(mSLog,
+                             "Collected virtual node {} of {} of component {}",
                              virtualNode, node, pComp->name());
         }
       }
